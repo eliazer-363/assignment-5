8b943109f008e4b49db54ebd9d39e4b48bec2535
eliazer-363 <84255271+eliazer-363@users.noreply.github.com>
2021-08-19 02:43:50 -0400

    Add files via upload

---
+++ car.js
@@ -0,0 +1,69 @@
+getcar=()=>{
+    let url='https://bluecollarsclientwork.000webhostapp.com/carbrowser/getcars.php';
+    let xhr=new XMLHttpRequest();
+    xhr.open('GET',url,true);
+    xhr.onload=()=>{
+        if(xhr.readyState==4||xhr.status==200)
+        {
+        let carbody=document.querySelector('.car');
+        let response=JSON.parse(xhr.responseText);
+        let {car}=response;
+        console.log(xhr.responseText);
+        car.map(car=>{
+            let car1=document.createElement('h2');
+            let carname=document.createTextNode(car.name);
+            let car2=document.createElement('h3');
+            let companyname=document.createTextNode(car.company);
+            let car3=document.createElement('p');
+            let statusname=document.createTextNode(car.status);
+
+
+            let deletebtn=document.createElement('button');
+            let deletebtntxt=document.createTextNode('delete');
+            deletebtn.appendChild(deletebtntxt);
+
+
+            car1.appendChild(carname);
+            car2.appendChild(companyname);
+            car3.appendChild(statusname);
+
+
+            carbody.appendChild(car1);
+            carbody.appendChild(car2);
+            carbody.appendChild(car3);
+             
+            let url2='https://bluecollarsclientwork.000webhostapp.com/carbrowser/getcars.php';
+            deletebtn.addEventListener('click',(e)=>{
+                e.preventDefault();
+                let ID=car.ID;
+                let data={
+                    "ID":ID,
+        "name":name,
+        
+        "company":company,
+        
+        "Status":Status,
+
+                };
+                let params=JSON.stringify(data);
+                let xhr=new XMLHttpRequest();
+                xhr.open('DELETE',url2,true);
+                xhr.onload=()=>{
+                    if(xhr.status==200||xhr.readyState==4)
+                    {
+                        console.log('Artist deleted');
+                    }
+                    else
+                    {
+                        console.log('Error occured');
+                    }
+                }
+                xhr.send(params);
+            });
+        });
+        }
+        
+    }
+    xhr.send();
+}
+getcar();
